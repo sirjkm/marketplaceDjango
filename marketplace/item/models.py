3c56bd8a91346f802b0e59c0a9302ccb029cb16c
Jason Manning <jasonkylemanning@gmail.com>
2024-04-17 11:48:38 -0400

    update models

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
